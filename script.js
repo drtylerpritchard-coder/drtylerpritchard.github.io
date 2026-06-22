97c66bb8b5f4f74106cbee4794ec6f11870eaf16
drtylerpritchard-coder <drtylerpritchard@gmail.com>
2026-06-22 08:56:02 -0230

    fgb

--- script.js
+++ script.js
@@ -30,3 +30,34 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+//site header
+
+window.addEventListener("scroll", function () {
+  const header = document.querySelector(".site-header");
+
+  if (window.scrollY >= 100) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
+
+//hideden and show
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+      entry.target.classList.remove("hidden");
+    } else {
+      entry.target.classList.remove("show");
+      entry.target.classList.add("hidden");
+    }
+  });
+}, {
+  threshold: 0.2 // trigger when 10% visible
+});
+
+const elements = document.querySelectorAll(".hidden");
+elements.forEach(el => observer.observe(el));
