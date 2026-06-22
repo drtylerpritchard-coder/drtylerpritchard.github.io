a3dcf43d03bdfd4b09c7733513535b27f607f011
drtylerpritchard-coder <drtylerpritchard@gmail.com>
2026-06-22 10:00:25 -0230

    asc

--- script.js
+++ script.js
@@ -11,26 +11,6 @@ document.addEventListener('DOMContentLoaded', () => {
   window.addEventListener('resize', setHeaderHeight);
 });
 
-//FAQ ACCORDIAN
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
-
 //site header
 
 window.addEventListener("scroll", function () {
@@ -42,7 +22,6 @@ window.addEventListener("scroll", function () {
     header.classList.remove("scrolled");
   }
 });
-
 
 //hideden and show
 const observer = new IntersectionObserver((entries) => {
@@ -61,3 +40,4 @@ const observer = new IntersectionObserver((entries) => {
 
 const elements = document.querySelectorAll(".hidden");
 elements.forEach(el => observer.observe(el));
+
